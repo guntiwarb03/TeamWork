cd662583cd32d930e884b5f4e80c86975e6ada02
guntiwarb03 <guntiwarb03@gmail.com>
2026-06-07 08:15:23 +0530

    This is secong commit

--- src/Login/RevNumPractice.java
+++ src/Login/RevNumPractice.java
@@ -2,6 +2,10 @@ package Login;
 
 public class RevNumPractice {
 public static void main(String[] args) {
+	String str = "Ananya";
+	for(int i=str.length()-1;i>=0;i--) {
+		System.out.println(str.charAt(i));
+	}
 	System.out.println("This branch belongs to bhagyashri");
 }
 }
